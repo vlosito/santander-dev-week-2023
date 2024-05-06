93080071ef5cee9b4d7e53d92c1924c787f49d28
Victor Losito <vlosito@gmail.com>
2024-05-06 20:47:49 -0300

    Implementação camada de dominio

--- src/test/java/me/dio/ApplicationTests.java
+++ src/test/java/me/dio/ApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.santanderdevweek2023;
+package me.dio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantanderDevWeek2023ApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
